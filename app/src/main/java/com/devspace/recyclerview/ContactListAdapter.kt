ffdae4b65a4e30fa5719ca4c2a64ef64458edf74
eduardoagabes <eduardoagabes@hotmail.com>
2024-10-13 14:00:31 +0200

    RecyclerView

--- app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
+++ app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
@@ -1,7 +1,10 @@
 package com.devspace.recyclerview
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -9,15 +12,36 @@ import androidx.recyclerview.widget.RecyclerView
 class ContactListAdapter :
     ListAdapter<Contact, ContactListAdapter.ContactViewHolder>(ContactDiffUtils()) {
 
+    private lateinit var onClickListener: (Contact) -> Unit
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+        return ContactViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val contact = getItem(position)
+        holder.bind(contact, onClickListener)
     }
 
-    class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    fun setOnClickListener(onClick: (Contact) -> Unit){
+        onClickListener = onClick
+    }
+
+    class ContactViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        private val tvName = view.findViewById<TextView>(R.id.tv_name)
+        private val tvPhone = view.findViewById<TextView>(R.id.tv_phone)
+        private val image = view.findViewById<ImageView>(R.id.image)
+
+        fun bind(contact: Contact, onClick: (Contact) -> Unit) {
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
+            image.setImageResource(contact.icon)
+
+            view.setOnClickListener {
+                onClick.invoke(contact)
+            }
+        }
 
     }
 
--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.devspace.recyclerview
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -20,89 +24,81 @@ class MainActivity : AppCompatActivity() {
         }
 
         val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val ivList = findViewById<ImageView>(R.id.iv_list)
+        val ivGrid = findViewById<ImageView>(R.id.iv_grid)
+        val adapter = ContactListAdapter()
 
+
+
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(contacts)
+
+        ivGrid.setOnClickListener {
+            rvList.layoutManager = GridLayoutManager(this, 2)
+        }
+        ivList.setOnClickListener {
+            rvList.layoutManager = LinearLayoutManager(this)
+        }
+
+        adapter.setOnClickListener { contact ->
+        val intent = Intent(this, ContactDetailActivity::class.java)
+            intent.putExtra("name", contact.name)
+            intent.putExtra("phone", contact.phone)
+            intent.putExtra("icon", contact.icon)
+
+            startActivity(intent)
+        }
     }
 }
 
 private val contacts = listOf(
     Contact(
-        name = "Rebeca",
-        phone = "(55)11 98827-5683",
-        icon = R.drawable.sample1
+        name = "Rebeca", phone = "(55)11 98827-5683", icon = R.drawable.sample1
     ),
     Contact(
-        name = "Eduardo",
-        phone = "(55)11 98326-5683",
-        icon = R.drawable.sample2
+        name = "Eduardo", phone = "(55)11 98326-5683", icon = R.drawable.sample2
     ),
     Contact(
-        name = "Magali",
-        phone = "(55)11 99195-6563",
-        icon = R.drawable.sample3
+        name = "Magali", phone = "(55)11 99195-6563", icon = R.drawable.sample3
     ),
     Contact(
-        name = "Camila",
-        phone = "(55)11 99727-1473",
-        icon = R.drawable.sample4
+        name = "Cecilia", phone = "(55)11 99727-1473", icon = R.drawable.sample4
     ),
     Contact(
-        name = "Rafaela",
-        phone = "(55)11 97727-5153",
-        icon = R.drawable.sample5
+        name = "Ana", phone = "(55)11 97727-5153", icon = R.drawable.sample5
     ),
     Contact(
-        name = "Patricia",
-        phone = "(55)11 98727-2533",
-        icon = R.drawable.sample6
+        name = "Patricia", phone = "(55)11 98727-2533", icon = R.drawable.sample6
     ),
     Contact(
-        name = "Marilia",
-        phone = "(55)11 96727-6563",
-        icon = R.drawable.sample7
+        name = "Marilia", phone = "(55)11 96727-6563", icon = R.drawable.sample7
     ),
     Contact(
-        name = "Marlon",
-        phone = "(55)11 99227-6363",
-        icon = R.drawable.sample8
+        name = "Marlon", phone = "(55)11 99227-6363", icon = R.drawable.sample8
     ),
     Contact(
-        name = "Erick",
-        phone = "(55) 11 98722-5913",
-        icon = R.drawable.sample9
+        name = "Erick", phone = "(55) 11 98722-5913", icon = R.drawable.sample9
     ),
     Contact(
-        name = "Davi",
-        phone = "(55) 11 99277-9213",
-        icon = R.drawable.sample10
+        name = "Davi", phone = "(55) 11 99277-9213", icon = R.drawable.sample10
     ),
     Contact(
-        name = "Lorena",
-        phone = "(55) 11 97221-1523",
-        icon = R.drawable.sample11
+        name = "Lorena", phone = "(55) 11 97221-1523", icon = R.drawable.sample11
     ),
     Contact(
-        name = "Paulo",
-        phone = "(55) 11 96171-1223",
-        icon = R.drawable.sample12
+        name = "Paulo", phone = "(55) 11 96171-1223", icon = R.drawable.sample12
     ),
     Contact(
-        name = "Julia",
-        phone = "(55) 11 97225-4513",
-        icon = R.drawable.sample13
+        name = "Julia", phone = "(55) 11 97225-4513", icon = R.drawable.sample13
     ),
     Contact(
-        name = "Carlos",
-        phone = "(55) 11 99111-1293",
-        icon = R.drawable.sample14
+        name = "Carlos", phone = "(55) 11 99111-1293", icon = R.drawable.sample14
     ),
     Contact(
-        name = "Maria",
-        phone = "(55) 11 97453-1298",
-        icon = R.drawable.sample15
+        name = "Maria", phone = "(55) 11 97453-1298", icon = R.drawable.sample15
     ),
     Contact(
-        name = "Eduarda",
-        phone = "(55) 11 97753-1192",
-        icon = R.drawable.sample16
+        name = "Eduarda", phone = "(55) 11 97753-1192", icon = R.drawable.sample16
     ),
 )
